4f99dbfc0b1ccac3ee4ef24b041a8f58dc69f71c
Vinzenz Schöberle <vinzenzschoeberle@gmail.com>
2025-02-13 15:58:49 +0100

    Early setup

--- mesh-generator.jl
+++ mesh-generator.jl
@@ -1,4 +1,18 @@
 ###This file is supposed to conatain the module for the pre-processo
+using JLD2
+using FileIO
+
+module Blocks #Coordinates of a base block x,y,z
+    Cartesian = [a,b,c ]
+end
+
 module Generator
-    
+    tll = 1 #transmissionlinelength
+    # The geometries which are used:
+    #Tetrahdal = 
+    #Cartesian = 
+
+    function mesh()
+        
+    end
 end
