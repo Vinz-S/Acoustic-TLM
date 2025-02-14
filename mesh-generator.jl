428ebe3692a48c35f5970fd0cea8872843ea7488
Vinzenz Schöberle <vinzenzschoeberle@gmail.com>
2025-02-14 17:39:12 +0100

    The two main building blocks are written

--- mesh-generator.jl
+++ mesh-generator.jl
@@ -1,9 +1,17 @@
 ###This file is supposed to conatain the module for the pre-processo
 using JLD2
 using FileIO
+using GLMakie
 
 module Blocks #Coordinates of a base block x,y,z
-    Cartesian = [a,b,c ]
+    Cartesian = [(x,y,z) for x = 0:1, y = 0:1, z = 0:1]
+    Cartesian = [Cartesian[i] for i = eachindex(Cartesian)]
+    #Originally used half as big coordinates for the tetahedal, but scaled up so all values can be integers
+    #This might need to be adjusted later on with regards to transmission line lengths
+    Tetraheder = [(0,0,0); (2,2,0); (1,1,1); (2,0,2); (0,2,2)]
+    Tetraheder = [Tetraheder;[(Tetraheder[i][1]+2,Tetraheder[i][2]+2,Tetraheder[i][3]) for i = eachindex(Tetraheder)];
+    [(Tetraheder[i][1],Tetraheder[i][2]+2,Tetraheder[i][3]+2) for i = eachindex(Tetraheder)];
+    [(Tetraheder[i][1]+2,Tetraheder[i][2],Tetraheder[i][3]+2) for i = eachindex(Tetraheder)];]
 end
 
 module Generator
@@ -16,3 +24,11 @@ module Generator
         
     end
 end
+
+#= 
+#Visually seeing that the coordinates are correct:
+f = Figure()
+ax3d = Axis3(f[1,1], title = "Tetraheder points")
+scatter!(ax3d, Blocks.Tetraheder)
+save("tetraheder.png", f)
+f =#
