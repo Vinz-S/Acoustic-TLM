305540acbef6e865b8aeb8a804f45d5a788ad8f1
Vinzenz Schöberle <vinzenzschoeberle@gmail.com>
2025-06-22 00:19:26 +0200

    some more fft plot changes

--- post/Averages plotted.jl
+++ post/Averages plotted.jl
@@ -20,43 +20,59 @@ data_files = ["average fft C_shoe_dirac.jld2";
 colours = [blue, green, pink]
 resolutions = ["5", "10", "20"]
 
-width = 1600; height = 800
-fig = Figure(size = (width, height))
-    supertitle = Label(fig[0, 1:2], "Simulated room frequency responses", fontsize = 30, tellwidth = false)
+width = 1050; height = 800
+fig = Figure(size = (width, height), fontsize = 20)
+    supertitle = Label(fig[0, 1:1], "Shoe-box frequency responses", fontsize = 30, tellwidth = false)
 
-for (i, file) in enumerate(data_files)
+for (i, file) in enumerate(data_files[1:4])
     data = load("results/fft averages/" * file)
     Fs = data["Fs"]
     modes = data["modes"]
     avg_f = data["avg_f"]
-
-    if i <= 4
-        crystal = (i%2 == 0 ? "tetrahedral" : "cartesian")
-        signal = (i <= 2 ? "dirac" : "sine sweep")
-        ax = Axis(fig[i, 1],
-        title = "Signal: $signal\t Cavity: shoe-box\t Crystal: $crystal",
-        xscale = log10, xminorticksvisible = true, xminorgridvisible = true,
-        xminorticks = IntervalsBetween(5), ylabel = "Amplitude", xlabel = (i == 4 ? "Frequency [Hz]" : ""))
-    end
-    if i > 4
-        crystal = (i%2 == 0 ? "tetrahedral" : "cartesian")
-        signal = (i <= 6 ? "dirac" : "sine sweep")
-        ax = Axis(fig[i-4, 2],
-        title = "Signal: $signal\t Cavity: sphere\t Crystal: $crystal",
-        xscale = log10, xminorticksvisible = true, xminorgridvisible = true,
-        xminorticks = IntervalsBetween(5), xlabel = (i == 8 ? "Frequency [Hz]" : ""))
-    end
+    
+    crystal = (i%2 == 0 ? "tetrahedral" : "cartesian")
+    signal = (i <= 2 ? "dirac" : "sine sweep")
+    ax = Axis(fig[i, 1],
+    title = "Signal: $signal\t Cavity: shoe-box\t Crystal: $crystal\t ",
+    xscale = log10, xminorticksvisible = true, xminorgridvisible = true,
+    xminorticks = IntervalsBetween(5), ylabel = "Amplitude", xlabel = (i == 4 ? "Frequency [Hz]" : ""))
     plots = []
     for j in eachindex(Fs)
         push!(plots, stairs!(ax, Fs[j][2], avg_f[j], step=:center, color=colours[j]))
     end
     mode = vlines!(ax, modes, color = redish)
-    if i == 8 || i == 4
-    end
-    if i == 8
-        Legend(fig[5, 1:2], [plots;[mode]],
-        [["TLLs per Wavelength: $(resolutions[j])" for j in eachindex(Fs)];["Analytical modal frequencies"]], tellwidth = false, orientation = :horizontal)
+    if i == 4
+        Legend(fig[5, 1:1], [plots;[mode]],
+        [["TLLs per Wavelength: $(resolutions[j])" for j in eachindex(Fs)];["Analytical mode"]], tellwidth = false, orientation = :horizontal)
     end
 end
 # display(fig)
-save("results/plots/averages_plotted.pdf", fig, resolution = (width, height))
+save("results/plots/shoe_averages_plotted.pdf", fig, resolution = (width, height))
+
+fig = Figure(size = (width, height), fontsize = 20)
+    supertitle = Label(fig[0, 1:1], "Spherical frequency responses", fontsize = 30, tellwidth = false)
+for (i, file) in enumerate(data_files[5:8])
+    data = load("results/fft averages/" * file)
+    Fs = data["Fs"]
+    modes = data["modes"]
+    avg_f = data["avg_f"]
+    
+    crystal = (i%2 == 0 ? "tetrahedral" : "cartesian")
+    signal = (i <= 6 ? "dirac" : "sine sweep")
+    ax = Axis(fig[i, 1],
+    title = "Signal: $signal\t Cavity: shoe-box\t Crystal: $crystal\t ",
+    xscale = log10, xminorticksvisible = true, xminorgridvisible = true,
+    xminorticks = IntervalsBetween(5), ylabel = "Amplitude", xlabel = (i == 8 ? "Frequency [Hz]" : ""))
+    plots = []
+    for j in eachindex(Fs)
+        push!(plots, stairs!(ax, Fs[j][2], avg_f[j], step=:center, color=colours[j]))
+    end
+    mode = vlines!(ax, modes, color = redish)
+    if i == 4
+        Legend(fig[5, 1:1], [plots;[mode]],
+        [["TLLs per Wavelength: $(resolutions[j])" for j in eachindex(Fs)];["Analytical mode"]], tellwidth = false, orientation = :horizontal)
+    end
+end
+
+
+save("results/plots/sphere_averages_plotted.pdf", fig, resolution = (width, height))
